c638f9fd910d76fca820bf893b8a0f6e5d479c18
Tommy McG <thomasjmcgovern98@gmail.com>
2022-04-18 15:57:13 -0400

    very basic page template

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-const myArr = ["hello", "there", "dude"];
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
-// printProfileData is function name, profileDataArr is name of input to function
-// forEach says do for loop on each item, profileItem is the name of each item at an index, and after the arrow is what we do with that profileItem 
-const printProfileData = profileDataArr => profileDataArr.forEach(profileItem => console.log(profileItem));
-  
-printProfileData(profileDataArgs);
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
